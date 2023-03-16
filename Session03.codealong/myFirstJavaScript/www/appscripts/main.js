3f6309b23baf9c1d049d16fa589737bdab2bc80e
bebinjoseph-nm2207 <e0726043@u.nus.edu>
2023-03-16 18:09:31 +0800

    Revert "testing"
    This reverts commit d83ba8b71555cc3446752b053645889fb8383921.

--- Session03.codealong/myFirstJavaScript/www/appscripts/main.js
+++
@@ -1,5 +0,0 @@
-// prompt("yo");
-var name = "Bebin";
-var job = "Student";
-// alert(name + " is a " + job) prompts are torture
-document.getElementById("colorDisplay").innerHTML= name + " is a " + job;
